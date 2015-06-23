72b6c31cdff995ec855351f257228d7c0f24951e
Dimitri John Ledkov <dimitri.j.ledkov@intel.com>
2015-06-23 11:50:13 +0100

    core: do not apply presets.

--- src/core/main.c
+++ src/core/main.c
@@ -1646,14 +1646,6 @@ int main(int argc, char *argv[]) {
 
         if (arg_running_as == MANAGER_SYSTEM) {
                 bump_rlimit_nofile(&saved_rlimit_nofile);
-
-                if (empty_etc) {
-                        r = unit_file_preset_all(UNIT_FILE_SYSTEM, false, NULL, UNIT_FILE_PRESET_ENABLE_ONLY, false, NULL, 0);
-                        if (r < 0)
-                                log_warning_errno(r, "Failed to populate /etc with preset unit settings, ignoring: %m");
-                        else
-                                log_info("Populated /etc with preset unit settings.");
-                }
         }
 
         r = manager_new(arg_running_as, arg_action == ACTION_TEST, &m);
